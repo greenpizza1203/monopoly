dfb68f730129025e08432eaa2e0888ee0f721f5b
greenpizza1203 <50718974+greenpizza1203@users.noreply.github.com>
2020-02-07 10:09:03 -0500

    Update Property.ts

--- common/properties/Property.ts
+++ common/properties/Property.ts
@@ -4,11 +4,7 @@ export class Property extends Schema {
     @type("string") name: string;
     @type("string") color: string;
     @type("number") cost: string;
-    @type("number") site: string;
-    @type("number") house_1: string;
-    @type("number") house_2: string;
-    @type("number") house_3: string;
-    @type("number") hotel: string;
+    @type(["number"]) rentCost: ArraySchema<number>();
 
     constructor(options: any) {
         super();
@@ -16,10 +12,10 @@ export class Property extends Schema {
         this.name = options.name;
         this.color = options.color;
         this.cost = options.cost;
-        this.site = options.site;
-        this.house_1 = options.house_1;
-        this.house_2 = options.house_2;
-        this.house_3 = options.house_3;
-        this.hotel = options.hotel;
+        this.rentCost.push(options.site);
+        this.rentCost.push(options.house_1);
+        this.rentCost.push(options.house_2);
+        this.rentCost.push(options.house_3);
+        this.rentCost.push(options.hotel);
     }
 }
