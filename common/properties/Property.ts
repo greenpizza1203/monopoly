fc5e15b4e35bb3aa6030b0cea0d5b02502720d55
greenpizza1203 <50718974+greenpizza1203@users.noreply.github.com>
2020-02-07 10:18:10 -0500

    Create Property.ts

---
+++ common/properties/Property.ts
@@ -0,0 +1,16 @@
+import {Schema, type} from "@colyseus/schema"
+
+export class Street extends Property {
+    @type(["number"]) rentCost: ArraySchema<number>();
+    @type("number") houseLevel:number=0;
+
+    constructor(options: any) {
+        super(options);
+        
+        this.rentCost.push(options.site);
+        this.rentCost.push(options.house_1);
+        this.rentCost.push(options.house_2);
+        this.rentCost.push(options.house_3);
+        this.rentCost.push(options.hotel);
+    }
+}
